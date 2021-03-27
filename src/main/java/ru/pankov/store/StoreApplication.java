ae30adce9ff6f8a73693bbd1b2d9919fecb99f5d
Maxim <adm.mjkee@yandex.ru>
2021-03-27 11:30:10 +0300

    remove soap

--- src/main/java/ru/pankov/store/StoreApplication.java
+++ src/main/java/ru/pankov/store/StoreApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-import java.util.Scanner;
-
 @SpringBootApplication
 @PropertySource("classpath:secret.properties")
 public class StoreApplication {
